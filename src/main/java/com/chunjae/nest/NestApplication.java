904653817f37ad487702ed0cca588bc766fc5256
young1the <youngthedev@gmail.com>
2023-10-06 10:05:52 +0900

    init: spring boot build

---
+++ src/main/java/com/chunjae/nest/NestApplication.java
@@ -0,0 +1,13 @@
+package com.chunjae.nest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NestApplication.class, args);
+    }
+
+}
