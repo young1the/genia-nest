505dfd0875d6c9839247ceabef617965e52df3a0
suyeon0000 <suyeon011@gmail.com>
2023-10-19 17:43:13 +0900

    ocr 페이지 레이아웃 구성(퍼블리싱 파일 사용하여 수정작업중)

--- react/src/ocr-main.jsx
+++ react/src/ocr-main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import OCRApp from "./OCRApp.jsx";
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <OCRApp />
