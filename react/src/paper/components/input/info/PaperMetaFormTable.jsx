23f97782615ac0e07225fb681331e96a673088ea
young1the <youngthedev@gmail.com>
2023-10-23 00:04:21 +0900

    feat: 정보 입력 인풋 태그 추가

--- react/src/paper/components/input/info/PaperMetaFormTable.jsx
+++ react/src/paper/components/input/info/PaperMetaFormTable.jsx
@@ -5,16 +5,44 @@ const PaperMetaFormTable = () => {
         <table className={styles.table}>
             <tbody className={styles.tbody}>
             <tr className={styles.tr}>
-                <th className={styles.th}>이름</th>
-                <td className={styles.td}>김두환</td>
+                <th className={styles.th}>기출년도</th>
+                <td className={styles.td}><input type={"number"} defaultValue={2023}/></td>
             </tr>
             <tr className={styles.tr}>
-                <th className={styles.th}>상세분류명</th>
-                <td className={styles.td}>김두환</td>
+                <th className={styles.th}>과목</th>
+                <td className={styles.td}><input type={"text"}/></td>
             </tr>
             <tr className={styles.tr}>
-                <th className={styles.th}>이름</th>
-                <td className={styles.td}>김두환</td>
+                <th className={styles.th}>학년</th>
+                <td className={styles.td}><input type={"text"}/></td>
+            </tr>
+            <tr className={styles.tr}>
+                <th className={styles.th}>학기</th>
+                <td className={styles.td}><input type={"text"}/></td>
+            </tr>
+            <tr className={styles.tr}>
+                <th className={styles.th}>시험종류</th>
+                <td className={styles.td}><input type={"text"}/></td>
+            </tr>
+            <tr className={styles.tr}>
+                <th className={styles.th}>총문제수</th>
+                <td className={styles.td}><input type={"text"}/></td>
+            </tr>
+            <tr className={styles.tr}>
+                <th className={styles.th}>지역</th>
+                <td className={styles.td}><input type={"text"}/></td>
+            </tr>
+            <tr className={styles.tr}>
+                <th className={styles.th}>학교급</th>
+                <td className={styles.td}><input type={"text"}/></td>
+            </tr>
+            <tr className={styles.tr}>
+                <th className={styles.th}>출판사</th>
+                <td className={styles.td}><input type={"text"}/></td>
+            </tr>
+            <tr className={styles.tr}>
+                <th className={styles.th}>학교명</th>
+                <td className={styles.td}><input type={"text"}/></td>
             </tr>
             </tbody>
         </table>
