8567ad7517c8ca28154df64687d2cbfeda3f81ff
young1the <youngthedev@gmail.com>
2023-10-27 15:23:29 +0900

    feat: 서버로 부터 pdf 받아오기

--- react/src/paper-main.jsx
+++ react/src/paper-main.jsx
@@ -4,7 +4,5 @@ import PaperApp from "./paper/PaperApp.jsx";
 import "./paper.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <PaperApp />
-  </React.StrictMode>,
 )
--- react/src/paper/PaperApp.jsx
+++ react/src/paper/PaperApp.jsx
@@ -4,7 +4,7 @@ import H1 from "./components/common/H1.jsx";
 import InfoSection from "./components/input/info/InfoSection.jsx";
 import PDFSection from "./components/input/pdf/PDFSection.jsx";
 import usePDF from "../pdf/hooks/usePDF.js";
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 
 function PaperApp() {
     const inputRefs = useRef();
@@ -18,6 +18,23 @@ function PaperApp() {
         generatePDFFile,
         pdfBlob,
     } = usePDF();
+    useEffect(() => {
+        getInitialValue();
+    }, []);
+    const getInitialValue = async () => {
+        let url = new URL(window.location.href);
+        let params = new URLSearchParams(url.search);
+        if (!params.has("id")) {
+            return ;
+        } else {
+            const response = await fetch(`/api/paper/detail/${params.get("id")}`);
+            const result = await response.json();
+            if (result.url) {
+                await addPDF({name:result.name, url:result.url});
+            }
+            console.log(result);
+        }
+    }
 
     const paperFileSubmitHandler = async () => {
         if (!pdfBlob) return ;
@@ -47,6 +64,7 @@ function PaperApp() {
         pdfFileUrl: pdfBlob,
         paperFileSubmitHandler,
     }
+
     return (
         <main className={styles.wrapper}>
             <header className={styles.header}>
