eed4f619ed5a8cc3e0729c29a55d795e7e40c270
최세진 <osaejino123@gmail.com>
2023-11-02 12:50:44 +0900

    feat: ocr 엑셀파일 다운로드 완료

--- src/main/java/com/chunjae/nest/domain/question/controller/QuestionController.java
+++ src/main/java/com/chunjae/nest/domain/question/controller/QuestionController.java
@@ -1,8 +1,11 @@
 package com.chunjae.nest.domain.question.controller;
 
+import com.chunjae.nest.common.excel.ExcelFile;
+import com.chunjae.nest.domain.paper.dto.PaperExcelDTO;
 import com.chunjae.nest.domain.paper.dto.SearchKeywordDTO;
 import com.chunjae.nest.domain.paper.entity.Paper;
 import com.chunjae.nest.domain.question.service.QuestionService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,6 +17,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,7 +41,6 @@ public class QuestionController {
         Page<Paper> papers = questionService.searchOCR(searchKeywordDTO, pageable);
         model.addAttribute("papers", papers);
         model.addAttribute("sizes", sizes);
-        //model.addAttribute("page", papers);
 
         // 페이징
         String ocrPageLink = "/ocr?";
@@ -104,6 +111,68 @@ public class QuestionController {
         model.addAttribute("ocrPageLink", ocrPageLink + "page=");
 
         return "pages/ocr/done";
+    }
+
+    @GetMapping("/download")
+    public void download(@ModelAttribute SearchKeywordDTO searchKeywordDTO, HttpServletResponse response){
+        Pageable adjustedPageable = PageRequest.of(0, Integer.MAX_VALUE);
+        Page<Paper> papers = questionService.searchOCR(searchKeywordDTO, adjustedPageable);
+
+        try {
+            List<PaperExcelDTO> cells = papers.stream().map(paper -> PaperExcelDTO.
+                    builder()
+                    .id(paper.getId())
+                    .year(paper.getYear())
+                    .month(paper.getMonth())
+                    .category(paper.getCategory())
+                    .grade(paper.getGrade())
+                    .area(paper.getArea())
+                    .subject(paper.getSubject())
+                    .name(paper.getName())
+                    .userId(paper.getUser().getName())
+                    .createdAt(paper.getCreatedAt())
+                    .paperStatus(paper.getPaperStatus())
+                    .build()).toList();
+            ExcelFile<PaperExcelDTO> paperExcelFile = new ExcelFile<>(cells, PaperExcelDTO.class);
+            response.setHeader("Content-Disposition", "attachment;filename=" +
+                    URLEncoder.encode(System.currentTimeMillis() + ".xlsx", StandardCharsets.UTF_8) + ";");
+            OutputStream outputStream = response.getOutputStream();
+            paperExcelFile.write(outputStream);
+            outputStream.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @GetMapping("/done/download")
+    public void doneDownload(@ModelAttribute SearchKeywordDTO searchKeywordDTO, HttpServletResponse response){
+        Pageable adjustedPageable = PageRequest.of(0, Integer.MAX_VALUE);
+        Page<Paper> papers = questionService.searchOCRDone(searchKeywordDTO, adjustedPageable);
+
+        try {
+            List<PaperExcelDTO> cells = papers.stream().map(paper -> PaperExcelDTO.
+                    builder()
+                    .id(paper.getId())
+                    .year(paper.getYear())
+                    .month(paper.getMonth())
+                    .category(paper.getCategory())
+                    .grade(paper.getGrade())
+                    .area(paper.getArea())
+                    .subject(paper.getSubject())
+                    .name(paper.getName())
+                    .userId(paper.getUser().getName())
+                    .createdAt(paper.getCreatedAt())
+                    .paperStatus(paper.getPaperStatus())
+                    .build()).toList();
+            ExcelFile<PaperExcelDTO> paperExcelFile = new ExcelFile<>(cells, PaperExcelDTO.class);
+            response.setHeader("Content-Disposition", "attachment;filename=" +
+                    URLEncoder.encode(System.currentTimeMillis() + ".xlsx", StandardCharsets.UTF_8) + ";");
+            OutputStream outputStream = response.getOutputStream();
+            paperExcelFile.write(outputStream);
+            outputStream.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @GetMapping("/transform")
